e6d0eb32c7a6acbcae3b63a3825751eb10ee4d53
feo3o <feo3o.me@gmail.com>
2025-04-29 14:05:48 -0300

    First Commit - Project update, add all files

---
+++ src/test/java/com/feo3o/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feo3o.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
